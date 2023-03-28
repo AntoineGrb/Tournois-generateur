0c20b6f2a8bec0cbd814a9e22c2f291a5c2c3938
Antoine Grubert <antoine_grubert@hotmail.fr>
2023-03-28 11:12:01 +0200

    JS quasi terminé

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const teamsSelect = document.querySelector("#teams-count");
-let teamsName = [];
-
+let teamsArray = [];
+let teamsCount = 4; //Valeur défaut
 
 //#region CREATION DES INPUTS NOMS DES EQUIPES
     //Fonction créer les inputs
@@ -44,7 +44,7 @@ let teamsName = [];
                 for (let team = 1 ; team <= 2 ; team++) {
                     let teamDiv = document.createElement("div");
                     teamDiv.setAttribute("class" , "tree__container__round__match__team");
-                    teamDiv.setAttribute("id" , `r${round}-m${match}-${team}`);
+                    teamDiv.setAttribute("id" , `r${round}-m${match}-t${team}`);
                     teamDiv.appendChild(document.createElement("p"));
                     matchDiv.appendChild(teamDiv);
 
@@ -52,6 +52,11 @@ let teamsName = [];
                         teamDiv.classList.add(`team__${teamTag}`);
                         teamTag++;
                     }
+
+                    //Ajouter l'écouteur d'évènement sur chaque matchDiv
+                    teamDiv.addEventListener("click", (e) => {
+                        clickToWin(e); //Définie hors de l'évent
+                    });
                 }
                 roundDiv.appendChild(matchDiv);
             }
@@ -60,30 +65,34 @@ let teamsName = [];
     }
 //#endregion
 
-//#region EVENT : CREER LES INPUTS ET L'ARBRE
-    let teamsCount = 4;
-
+//EVENT : CREER LES INPUTS ET L'ARBRE
     teamsSelect.addEventListener("change" , (e) => {
         e.preventDefault();
         teamsCount = e.target.value;
         createInputs(teamsCount);
         createTree(teamsCount);
     })
-//#endregion
 
-//#region VALIDATION DU TOURNOI
-    //Event : sélection du mode
+//#region VALIDER LE TOURNOI 
+    //Sélection du mode
     let mode = "order";
     const drawMode = document.querySelector("#draw-mode");
     drawMode.addEventListener("change" , (e) => {
         mode = e.target.value;
     });
 
-    //Event : valider le tournoi
-    document.querySelector("#button__valider").addEventListener("click" , () => {
+    //Event : Cliquer sur Valider
+    document.querySelector("#button__valider").addEventListener("click" , (e) => {
+
+            //Contrôle des champs Inputs
+            for (let i = 1 ; i <= teamsCount ; i++) {
+                let inputChecked = document.querySelector(`#input-team${i}`);
+                if (inputChecked.value.trim() === "") {
+                    return alert("Toutes les équipes n'ont pas de nom !");
+                }
+            }
 
             //Stock les valeurs des inputs dans un tableau
-            let teamsArray = [];
             for (let team = 1 ; team <= teamsCount ; team++) {
                 const teamInputName = document.querySelector(`#input-team${team}`).value;
                 teamsArray.push(teamInputName);
@@ -100,14 +109,83 @@ let teamsName = [];
                     return array;
                 }
                 shuffle(teamsArray);
-                console.log("arrayRandom = " , teamsArray);
             };
-            //Draw
+
+            //Tirage
             for (let team = 1 ; team <= teamsCount ; team++) {
                 const teamDivName = document.querySelector(`.team__${team} > p`);
                 teamDivName.innerText = teamsArray[team - 1];
             };
-            //! Rajouter une alerte si tous les inputs ne sont pas complétés
+
+            //Désactive la sélection des paramètres une fois validé
+            e.target.style.display = "none";
+            document.querySelector("#teams-count").disabled = true; 
+            document.querySelector("#tournament-name").disabled = true;
+            document.querySelector("#draw-mode").disabled = true;
+            document.querySelector("#button__annuler").style.display = "block"; //Affiche le bouton Annuler 
+    });
+
+//#endregion
+
+//#region ANNULER LE TOURNOI
+
+    //Event : Cliquer sur Annuler
+    document.querySelector("#button__annuler").addEventListener("click" , (e) => {
+        teamsArray = [];
+        createInputs(4);
+        createTree(4);
+
+        //Réactive la sélection des paramètres et affiche le bouton Valider
+        e.target.style.display = "none";
+        document.querySelector("#teams-count").disabled = false; 
+        document.querySelector("#tournament-name").disabled = false;
+        document.querySelector("#tournament-name").value = "";
+        document.querySelector("#draw-mode").disabled = false;
+        document.querySelector("#button__valider").style.display = "block";   
+    });
+
+//#endregion
+
+//#region DETERMINER LE VAINQUEUR D'UN MATCH ET PASSAGE AU TOUR SUIVANT
+    //Fonction : cliquer pour passer au prochain tour
+    function clickToWin(e) {
+        // On récupère les infos avec l'ID de la div sélectionnée
+        const currentId = e.currentTarget.getAttribute("id");
+        const currentRound = parseInt(currentId.slice(1,2));
+        const currentMatch = parseInt(currentId.slice(4,5));
+        const currentTeam = parseInt(currentId.slice(7,8));
+        let currentOtherTeam = 1;
+        if (currentTeam === 1) { //L'équipe perdante
+            currentOtherTeam = 2;
+        } 
+
+        //On détermine l'ID de la div du tour suivant
+        const nextRound = currentRound + 1;
+        const nextMatch = Math.ceil(currentMatch / 2);
+        let nextTeam = 1; 
+        if (currentMatch % 2 === 0 ) {
+            nextTeam = 2; //Si le match est un nombre pair, alors l'équipe ira en position 2 au prochain match
+        };
+        const nextId = `r${nextRound.toString()}-m${nextMatch.toString()}-t${nextTeam.toString()}`;
+        console.log("ID winning team " ,nextId);
+
+        //On envoie l'équipe au tour suivant
+        const winnerTeam = e.currentTarget.querySelector("p").innerText;
+        document.querySelector(`#${nextId} > p `).innerText = winnerTeam;
+
+        //On met en gras le vainqueur
+        e.currentTarget.style.fontWeight = "bold";
+        //On s'assure que l'autre équipe n'est pas en gras
+        const currentOtherTeamId = `r${currentRound.toString()}-m${currentMatch.toString()}-t${currentOtherTeam.toString()}`;
+        document.getElementById(currentOtherTeamId).style.fontWeight = "normal";
+    };
+
+    //Event : cliquer sur une div pour passer au prochain tour
+    const teamsDiv = document.querySelectorAll("div[id^=r][id$=t1], div[id^=r][id$=t2]");   
+    teamsDiv.forEach(teamDiv => {
+        teamDiv.addEventListener("click", (e) => {
+            clickToWin(e);
+        });
     });
 
 //#endregion
